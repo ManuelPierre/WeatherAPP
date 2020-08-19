e11d3968f1c468463ef46d29349bc36e826761d4
ManuelPierre <66449693+ManuelPierre@users.noreply.github.com>
2020-08-19 16:37:12 -0400

    Final Changes

--- assets/script.js
+++ assets/script.js
@@ -1,204 +1,165 @@
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=36732039e96fa8554b899ed82cae96fb
+var cities = [];
 
-// function buildQueryURL() {
-//     // queryURL is the url we'll use to query the API
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?";
+$(".searchBtn").on("click", getWeatherSearchBtn);
+$(document).on("click", ".city-btn", getWeatherBtnClick);
 
-//     // Begin building an object to contain our API call's query parameters
-//     // Set the API key
-//     var queryParams = { "APPID": "524901&APPID=36732039e96fa8554b899ed82cae96fb" };
+function getWeatherBtnClick() {
+  event.preventDefault();
+  var city = $(this).attr("data-name");
+  $("#search").val(city);
+  getWeather();
+}
 
-$(".searchBtn").on("click", function (event) {
+function getWeatherSearchBtn() {
+  var cityName = $("#search").val();
+  var btn = $("<button>").text(cityName);
+  cities.push(cityName);
+  btn.attr("data-name", cityName);
+  btn.addClass("city-btn");
+  var stringifyCity = JSON.stringify(cities);
+  localStorage.setItem("cities", stringifyCity);
+  $("#results").append(btn);
+  getWeather();
+}
 
-  console.log("called")
+function getWeather() {
+  console.log("called");
 
   event.preventDefault();
 
-
   var terms = $("#search").val();
 
-
-
-
-  //var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + terms + "&appid=36732039e96fa8554b899ed82cae96fb";
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + terms + "&appid=09dc7618764f3173b089191eb7e172a1";
-  // id=524901        
-  //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
-  // 36732039e96fa8554b899ed82cae96fb
-  //api.openweathermap.org/data/2.5/forecast?q=Atlanta&appid=36732039e96fa8554b899ed82cae96fb
-
-
-
-
-
-
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    terms +
+    "&units=imperial&appid=09dc7618764f3173b089191eb7e172a1";
 
   $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
+    var cityTempe = response.list[0].main.temp;
+    console.log(`<p>${cityTempe} </p>`);
 
+    var cityHumid = response.list[0].main.humidity;
+    console.log(`<p>${cityHumidity} </p>`);
 
+    var cityWinde = response.list[0].wind.speed;
+    console.log(`<p>${cityWind} </p>`);
 
-    .then(function (response) {
+    //   !template
+    // for destructuring
+    var names = ["alpha", "beta", "gamma", "delta"];
 
-      // Log the queryURL
-      console.log(response);
-      // console.log("hi");
-      //! THIS WORKS => console.log(response.city)
-      // console.log(response.city.name)
-      // console.log(response.city)
-      // console.log(typeof(response.city))
+    var firstName = names[0];
+    var secondName = names[1];
 
-      // for (let i = 0; i <response.city.length; i++) {
-      // console.log('run')
-      //! for (const city in response.city) {
-      // for (const property in object) {
-      // console.log(`${property}: ${object[property]}`);
-      //! console.log(city)
-      // console.log(`${property}: ${object[property]}`);
-      var cityName = response.city.name;
-      console.log(`<p>${cityName} </p>`)
+    console.log(firstName);
+    console.log(secondName);
 
-      // var cityDat = response.list[0].dt_txt;
-      // console.log(`<p>${cityDat}</p>`)
+    var cityTitle = $("#city-title").get(0);
+    cityTitle.innerHTML = terms + " " + new Date().toLocaleDateString();
 
-      // this is the icon path:  list[0].weather[0].icon
-      // var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
-      // console.log(img)
+    var cityTemp = $("#city-temp").get(0);
+    cityTemp.innerHTML = "Temperature: " + cityTempe;
 
-      var cityTempe = response.list[0].main.temp;
-      console.log(`<p>${cityTempe} </p>`)
+    var cityHumidity = $("#city-humidity").get(0);
+    cityHumidity.innerHTML = "Humidity: " + cityHumid;
 
-      var cityHumid = response.list[0].main.humidity;
-      console.log(`<p>${cityHumidity} </p>`)
+    var cityWind = $("#wind-speed").get(0);
+    cityWind.innerHTML = "Wind: " + cityWinde;
 
-      var cityWinde = response.list[0].wind.speed;
-      console.log(`<p>${cityWind} </p>`)
+    var lat = response.city.coord.lat;
+    var lon = response.city.coord.lon;
+    var ApiKey = "09dc7618764f3173b089191eb7e172a1";
 
+    var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${ApiKey}`;
 
+    $.ajax({
+      url: oneCallApi,
+      method: "GET",
+    }).then(function (data) {
+      console.log(data);
 
+      var cityUvie = data.current.uvi;
+      $("#UV-index").text(cityUvie);
+      $(".forecast").empty();
 
-      // ? what do you want to do?
+      if (cityUvie < 3) {
+        $("#UV-index").css("background-color", "green");
+      } else if (cityUvie > 7) {
+        $("#UV-index").css("background-color", "red");
+      } else {
+        $("#UV-index").css("background-color", "yellow");
+      }
 
-      // ?1 create element using html then bind using jquery to use innerHTML to display cityName
+      for (let index = 1; index <= 5; index++) {
+        console.log(data.daily[index]);
+        var temp = data.daily[index].temp.day;
+        var hum = data.daily[index].humidity;
+        var date = moment.unix(data.daily[index].dt).format("MMMM Do YYYY");
+        console.log(date);
 
-      //  ?2 create element using js  then bind using jquery to use innerHTML to display cityName
+        var img = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/w/" +
+            response.list[0].weather[0].icon +
+            ".png"
+        );
 
-      // console.log(response.city.name)
-      //   var secondName = names[1];
+        var div = $("<div>");
 
-      //   console.log(firstName);//"alpha"
-      //   console.log(secondName);//"beta"
-      // }
+        var cardDiv = $("<div>").addClass("card");
+        cardDiv.css("background-color", "blue");
 
-      /* 
-                      ! template for destructuring
-                      var names = ["alpha", "beta", "gamma", "delta"]; 
+        div.addClass("card-body");
+        var p = $("<p>");
 
-                      var firstName = names[0];
-                      var secondName = names[1];
+        p.text(temp).addClass("card-title");
+        div.append(p);
 
-                      console.log(firstName);//"alpha"
-                      console.log(secondName);//"beta"
-                      */
+        div.append(img);
 
-      //  var cityTitle = $("#city-title").innerHTML(cityName);
+        var p2 = $("<p>");
 
+        p2.text(hum).addClass("card-text");
+        div.append(p2);
 
-      var cityTitle = $("#city-title").get(0);
-      cityTitle.innerHTML = cityName;
+        var p3 = $("<p>");
 
-      // var cityDate = $("#city-date").get(0);
-      // cityDate.innerHTML = ("(" + cityDat + ")");
-
-
-      var cityTemp = $("#city-temp").get(0);
-      cityTemp.innerHTML = ("Temperature: " + cityTempe);
-
-      var cityHumidity = $("#city-humidity").get(0);
-      cityHumidity.innerHTML = ("Humidity: " + cityHumid);
-
-      var cityWind = $("#wind-speed").get(0);
-      cityWind.innerHTML = ("Wind: " + cityWinde);
-
-
-      //  .textcontent(cityName);
-
-
-
-      var lat = response.city.coord.lat
-      var lon = response.city.coord.lon
-      var ApiKey = "09dc7618764f3173b089191eb7e172a1"
-
-
-      var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-      units=imperial&appid=${ApiKey}`
-
-      $.ajax({
-          url: oneCallApi,
-          method: "GET"
-        })
-
-        .then(function (data) {
-
-
-          console.log(data)
-
-
-
-
-          var cityUvie = data.current.uvi
-          $("#UV-index").text(cityUvie)
-          // create a condition to see if UV is severe moderate or high
-          if (cityUvie < 3) {
-            $("#UV-index").css('background-color', 'green')
-          } else if (cityUvie > 7) {
-            $("#UV-index").css('background-color', 'red')
-          } else {
-            $("#UV-index").css('background-color', 'yellow')
-          }
-
-          for (let index = 1; index <= 5; index++) {
-            console.log(data.daily[index])
-            var temp = data.daily[index].temp.day
-
-            var div = $("<div>")
-            div.addClass("card-body")
-            var p = $("<p>")
-
-            p.text(temp).addClass("card-title")
-            div.append(p)
-            $(".forecast").append(div)
-            //   $().text(temp)
-            //   $("#Day3").text(temp)
-            //   $("#Day4").text(temp)
-            // }
-            // $("#Day5").text(temp)
-          }
-
-
-
-
-
-          // var ptag = $("<p>")
-          // ptag.text(response.city[i].abstract)
-          // ptag.text(response.response.city[i].abstract)
-          // console.log(response.docs[i].abstract)
-          // console.log(response.response.docs[i].abstract)
-
-          // var ptag2 = $("<p>")
-          // ptag2.text(response.docs[i].pub_date)
-          // ptag2.text(response.response.docs[i].pub_date)
-
-          // $("#results").append(ptag)
-          // $("#results").append(ptag2)
-          // $("#results").append(ptag).append(ptag2)
-
-
-          // }
-        });
+        p3.text(date).addClass("card-text");
+        div.append(p3);
+        cardDiv.append(div);
+        $(".forecast").append(cardDiv);
+      }
     });
+  });
+}
 
-});
+// }
+
+function renderButtons() {
+  console.log("run");
+
+  cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+  for (var i = 0; i < cities.length; i++) {
+    var priorResults = [];
+
+    console.log("run");
+
+    var r = $("<button>");
+
+    r.addClass("city-btn");
+
+    r.attr("data-name", cities[i]);
+
+    r.text(cities[i]);
+
+    $("#results").append(r);
+  }
+}
+
+renderButtons();
